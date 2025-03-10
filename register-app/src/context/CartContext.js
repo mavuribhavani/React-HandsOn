06d7fe400f64c55d07e17df755dd201017db9d99
mavuribhavani <mavuribhavani63@gmail.com>
2025-03-10 18:45:38 +0530

    register-app committed

--- register-app/src/context/CartContext.js
+++ register-app/src/context/CartContext.js
@@ -6,16 +6,30 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    setCart((prevCart) => {
+      const isProductInCart = prevCart.find((item) => item.id === product.id);
+      if (isProductInCart) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
     alert(`${product.name} added to cart!`);
   };
 
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, totalPrice }}>
       {children}
     </CartContext.Provider>
   );
 };
 
-// Custom hook to use Cart Context
 export const useCart = () => useContext(CartContext);
